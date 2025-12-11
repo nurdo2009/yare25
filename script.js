5830e1883458c00a1b707b76056071d422a43e6c
nurdo2009 <wertbvjcc@gmail.com>
2025-12-11 12:17:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let now = new Date()
+
+let year = now.getFullYear()
+let month = now.getMonth()     
+let day = now.getDay()         
+let date = now.getDate()
+let hours = now.getHours()
+let minutes = now.getMinutes()
+let seconds = now.getSeconds()
+
+console.log(
+  "Азыркы жыл:", year,
+  ", Азыркы ай:", month,
+  ", жуманын күнү:", day,
+  ", Айдагы күнү:", date,
+  ", Саат:", hours,
+  ", Мүнөт:", minutes,
+  ", Секунд:", seconds,
+)
